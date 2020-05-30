4d270b277c3c845529cb8508f6be9798da04c6d9
anuraj-18 <shubham.anuraj.s@gmail.com>
2020-05-30 20:16:33 +0530

    Code cleanup with weights saving

--- facial_rec.py
+++ facial_rec.py
@@ -1,164 +1,87 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 14 15:00:58 2018
-
-@author: MAHE
-"""
 
 import numpy as np
-from scipy import misc
 import cv2
+from nn import ANN
 
-def sigmoid(a):
-    return 1/(1 + np.exp(-1 * a))
+def init_train_data():
+    X = []
+    for i in range(1, 11):
+        name = "images/shubham" + str(i) + ".jpg"
+        arr = cv2.imread(str(name)) # 640x480x3 array
+        arr = np.reshape(arr,(1,480*640*3))/255
+        X.append(arr[0])
+    for i in range(5):
+        name = "images/mahika" + str(i) + ".jpg"
+        arr = cv2.imread(name) # 640x480x3 array
+        arr = np.reshape(arr, (1, 480*640*3))/255
+        X.append(arr[0])
+    for i in range(1, 5):
+        name = "images/dad" + str(i)+".jpg"
+        arr = cv2.imread(name) # 640x480x3 array
+        arr = np.reshape(arr, (1,480*640*3))/255
+        X.append(arr[0])
 
-def initialize_parameters(layer_dims):
-    parameters={}
-    for i in range(0, len(layer_dims)-1):
-        #np.random.seed(7)
-        np.random.seed(4)
-        parameters["W" + str(i + 1)] = np.random.randn(layer_dims[i + 1],layer_dims[i])*1
-        parameters["b" + str(i + 1)] = np.zeros((layer_dims[i + 1], 1))
-    return parameters
-    
-def compute_cost(AL, Y):
-    m = Y.shape[1]
-    cost = 0.5 * (1/m) * np.sum(np.power(AL - Y, 2))
-    return np.squeeze(cost)
+    for i in range(5):
+        name = "images/mum" + str(i) + ".jpg"
+        arr = cv2.imread(name) # 640x480x3 array
+        arr = np.reshape(arr, (1,480*640*3))/255
+        X.append(arr[0])
 
-def forward_prop(X, parameters):
-    A = X
-    L = len(parameters)//2
-    cache = {}
-    for i in range(1, L+1):
-        Z = np.dot(parameters["W" + str(i)], A) + parameters["b" + str(i)]
-        cache["Z" + str(i)] = Z
-        A = sigmoid(Z)
-        cache["A" + str(i)] = A
-    return A, cache
+    X = np.array(X)
+    X = X.T
 
-def backward_prop(X, Y, parameters, cache):
-    grads = {}
-    L = len(parameters)//2
-    m = Y.shape[1]
-    AL = cache["A"+str(L)]
-    dZ = np.multiply(AL - Y, np.multiply(AL, 1 - AL))
-    grads["dW" + str(L)] = (1/m) * np.dot(dZ, cache["A" + str(L - 1)].T)
-    grads["db" + str(L)]=(1/m) * np.sum(dZ, axis = 1, keepdims = True)
-    cache["A0"] = X
-    for i in range(L-1, 0, -1):
-        y = cache["A" + str(i)]
-        dZL_1 = np.dot(parameters["W" + str(i + 1)].T, dZ) * (y - np.power(y, 2))
-        grads["dW" + str(i)] = (1/m) * np.dot(dZL_1, cache["A" + str(i - 1)].T)
-        grads["db" + str(i)] = (1/m) * np.sum(dZL_1, axis = 1, keepdims = True)
-        dZ = dZL_1
+    Y = []
+    for i in range(X.shape[1]):
+        if i<10:
+            k = [1, 0, 0, 0]
+            Y.append(k)
+        elif i >= 10 and i < 15:
+            k = [0, 1, 0, 0]
+            Y.append(k)
+        elif i >= 15 and i < 19:
+            k = [0, 0, 1, 0]
+            Y.append(k)
+        else:
+            k = [0, 0, 0, 1]
+            Y.append(k)
+    Y = np.array(Y)
+    Y = Y.T
+    return X, Y
 
-    return grads
-
-def update_parameters(parameters, grads, learning_rate):
-    L = len(parameters) // 2
-    for i in range(1, L + 1):
-        parameters["W" + str(i)] = parameters["W"+str(i)] - (learning_rate * grads["dW" + str(i)])
-        parameters["b" + str(i)] = parameters["b" + str(i)] - (learning_rate * grads["db" + str(i)])
-    return parameters
-
-def test_output(X, parameters, layer_dims):
-    A = X
-    for i in range(1, len(layer_dims)):
-        A = np.dot(parameters["W" + str(i)], A) + parameters["b" + str(i)]
-        A = sigmoid(A)
-    return A
-
-X = []
-
-for i in range(10):
-    name = "shubham" + str(i) + ".jpg"
-    arr = misc.imread(name) # 640x480x3 array
-    arr = np.array(arr) # 3-vector for a pixel
-    arr = np.reshape(arr,(1,480*640*3))/255
-    X.append(arr[0])
-for i in range(5):
-    name = "mahika" + str(i) + ".jpg"
-    arr = misc.imread(name) # 640x480x3 array
-    arr = np.array(arr) # 3-vector for a pixel
-    arr = np.reshape(arr, (1, 480*640*3))/255
-    X.append(arr[0]
-            )
-for i in range(1, 5):
-    name = "dad" + str(i)+".jpg"
-    arr = misc.imread(name) # 640x480x3 array
-    arr = np.array(arr) # 3-vector for a pixel
-    arr = np.reshape(arr, (1,480*640*3)/255)
-    X.append(arr[0])
-
-for i in range(5):
-    name = "mum" + str(i) + ".jpg"
-    arr = misc.imread(name) # 640x480x3 array
-    arr = np.array(arr) # 3-vector for a pixel
-    arr = np.reshape(arr, (1,480*640*3))/255
-    X.append(arr[0])
-
-X = np.array(X)
-X = X.T
-
-Y = []
-for i in range(X.shape[1]):
-    if i<10:
-        k = [1, 0, 0, 0]
-        Y.append(k)
-    elif i >= 10 and i < 15:
-        k = [0, 1, 0, 0]
-        Y.append(k)
-    elif i >= 15 and i < 19:
-        k = [0, 0, 1, 0]
-        Y.append(k)
-    else:
-        k = [0, 0, 0, 1]
-        Y.append(k)
-Y = np.array(Y)
-Y = Y.T
-print(Y)
-
-layer_dims = [480*640*3, 7, 7, Y.shape[0]]
-
-parameters = initialize_parameters(layer_dims)
-
-for i in range(301): #301 runs
-    AL, cache = forward_prop(X,parameters)
-    cost = compute_cost(AL,Y)
-    if i%10 == 0:
-        print("Cost after " + str(i) + ":" + str(cost))
-    grads = backward_prop(X, Y, parameters, cache)
-    parameters=update_parameters(parameters, grads, 1.5)
-print(AL)
-
-video = cv2.VideoCapture(0)
-d={0:"Shubham", 1:"Mahika", 2:"Raj", 3:"Anupma"}
-
-while True:
+def create_input_data(video):
     X = []
     check, frame = video.read()
     frame = np.reshape(frame, (1,480*640*3))
     X.append(frame[0])
     X = np.array(X)
     X = X.T
-    s = test_output(X,parameters,layer_dims)
-    m = np.amax(s)
-    print(s)
-    for i in range(len(s)):
-        if s[i][0] == m:
-            print("This is " + d[i])
+    return X
+
+def testOutput(nn):
+    video = cv2.VideoCapture(0)
+    d={0:"Shubham", 1:"Mahika", 2:"Raj", 3:"Anupma"}
+
+    while True:
+        X = create_input_data(video)
+        s = nn.test_output(X, layer_dims)
+        m = np.amax(s)
+        print(s)
+        for i in range(len(s)):
+            if s[i][0] == m:
+                print("This is " + d[i])
+                break
+
+        check, frame = video.read()
+        cv2.imshow("Capturing", frame)
+        key=cv2.waitKey(1)
+        
+        if key == ord("q"):
             break
-            
-    check, frame = video.read()
-    
-    cv2.imshow("Capturing", frame)
-    key=cv2.waitKey(1)
-    
-    if key == ord("q"):
-        break
-    
-video.release()
+    video.release()
+    cv2.destroyAllWindows()
 
-
-cv2.destroyAllWindows()
+layer_dims = [480*640*3, 7, 7, 4]
+X, Y = init_train_data()
+nn = ANN(layer_dims)
+nn.getWeights()
+testOutput(nn)
---
+++ nn.py
@@ -0,0 +1,85 @@
+import numpy as np
+import cv2
+import pickle
+
+class ANN:
+    def __init__(self, layers):
+        self.params = {}
+        for i in range(0, len(layers)-1):
+            #np.random.seed(7)
+            np.random.seed(4)
+            self.params["W" + str(i + 1)] = np.random.randn(layers[i + 1],layers[i])*1
+            self.params["b" + str(i + 1)] = np.zeros((layers[i + 1], 1))
+
+    def sigmoid(self, a):
+        return 1/(1 + np.exp(-1 * a))
+      
+    def compute_cost(self, AL, Y):
+        m = Y.shape[1]
+        cost = 0.5 * (1/m) * np.sum(np.power(AL - Y, 2))
+        return np.squeeze(cost)
+
+    def forward_prop(self, X):
+        A = X
+        L = len(self.params)//2
+        cache = {}
+        for i in range(1, L+1):
+            Z = np.dot(self.params["W" + str(i)], A) + self.params["b" + str(i)]
+            cache["Z" + str(i)] = Z
+            A = self.sigmoid(Z)
+            cache["A" + str(i)] = A
+        return A, cache
+
+    def backward_prop(self, X, Y, cache):
+        grads = {}
+        L = len(self.params)//2
+        m = Y.shape[1]
+        AL = cache["A"+str(L)]
+        dZ = np.multiply(AL - Y, np.multiply(AL, 1 - AL))
+        grads["dW" + str(L)] = (1/m) * np.dot(dZ, cache["A" + str(L - 1)].T)
+        grads["db" + str(L)]=(1/m) * np.sum(dZ, axis = 1, keepdims = True)
+        cache["A0"] = X
+        for i in range(L-1, 0, -1):
+            y = cache["A" + str(i)]
+            dZL_1 = np.dot(self.params["W" + str(i + 1)].T, dZ) * (y - np.power(y, 2))
+            grads["dW" + str(i)] = (1/m) * np.dot(dZL_1, cache["A" + str(i - 1)].T)
+            grads["db" + str(i)] = (1/m) * np.sum(dZL_1, axis = 1, keepdims = True)
+            dZ = dZL_1
+
+        return grads
+
+    def update_parameters(self, grads, learning_rate):
+        L = len(self.params) // 2
+        for i in range(1, L + 1):
+            self.params["W" + str(i)] = self.params["W"+str(i)] - (learning_rate * grads["dW" + str(i)])
+            self.params["b" + str(i)] = self.params["b" + str(i)] - (learning_rate * grads["db" + str(i)])
+
+    def test_output(self, X, layer_dims):
+        A = X
+        L = len(self.params) // 2
+        for i in range(1, L + 1):
+            A = np.dot(self.params["W" + str(i)], A) + self.params["b" + str(i)]
+            A = self.sigmoid(A)
+        return A
+
+    def train(self, X, Y):
+        for i in range(301): #301 runs
+            AL, cache = self.forward_prop(X)
+            cost = self.compute_cost(AL,Y)
+            if i%10 == 0:
+                print("Cost after " + str(i) + ":" + str(cost))
+            grads = self.backward_prop(X, Y, cache)
+            self.update_parameters(grads, 1.5)
+            
+        self.saveWeights()
+        print("Parameters saved in imgWeights.txt")
+
+    def saveWeights(self):
+        f = open("imgWts.txt", "wb")
+        pickle.dump(self.params, f)
+        f.close()
+
+    def getWeights(self):
+        f = open("imgWts.txt", "rb")
+        self.params = pickle.load(f)
+        f.close()
